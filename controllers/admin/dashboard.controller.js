7909efacbf2ad7070c6e102d067b1a78d289a81c
Trần Đức Trình <162284947+trinhoke@users.noreply.github.com>
2024-08-14 17:33:18 +0700

    update interface dashboard-page

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,5 +1,23 @@
-
+const User = require("../../models/user.model");
+const Product = require("../../models/product.model");
+const Order = require("../../models/order.model");
 // [GET] /admin/dashboard
-module.exports.index = (req, res) => {
-    res.render('admin/pages/dashboard/index');
+module.exports.index = async (req, res) => {
+    try {
+        const totalUsers = await User.countDocuments();
+        const totalProducts = await Product.countDocuments();
+        const totalOrders = await Order.countDocuments();
+        const totalRevenue = 25000; // Giả sử bạn có một cách để tính tổng doanh thu
+
+        res.render('admin/pages/dashboard/index', {
+            title: 'Dashboard',
+            totalUsers,
+            totalProducts,
+            totalOrders,
+            totalRevenue
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 }
