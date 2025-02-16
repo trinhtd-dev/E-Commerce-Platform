2c00362e727183d02ac0bd813265cbb2121f9ec0
Trần Đức Trình <162284947+trinhoke@users.noreply.github.com>
2025-02-16 21:49:12 +0700

    refactor: update user middleware to use JWT token verification

--- middlewares/user.middleware.js
+++ middlewares/user.middleware.js
@@ -1,13 +1,16 @@
 const User = require("../models/user.model");
 const Cart = require("../models/cart.model");
+const JWTService = require("../services/jwt.service");
 
-module.exports.user = async (req, res, next)  => {
-    if(req.cookies.userToken){
-        const user = await User.findOne({token: req.cookies.userToken});
-        if(user){
-            res.locals.user = user;
-            await Cart.updateOne({_id: req.cookies.cartId}, {userId: user.id});
-        }
+module.exports.user = async (req, res, next) => {
+  if (req.cookies.accessToken) {
+    const accessToken = req.cookies.accessToken;
+    const decoded = JWTService.verifyToken(accessToken);
+    const user = await User.findOne({ _id: decoded.userId });
+    if (user) {
+      res.locals.user = user;
+      await Cart.updateOne({ _id: req.cookies.cartId }, { userId: user.id });
     }
-    next();
-}
+  }
+  next();
+};
